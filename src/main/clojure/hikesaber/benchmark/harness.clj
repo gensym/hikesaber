96dd2ba27af46a75a9e225bdba28882628fad298
David Altenburg <daltenburg@drwholdings.com>
2015-06-02 19:23:51 -0500

    Moved data loading into setup of benchmark

--- src/main/clojure/hikesaber/benchmark/harness.clj
+++ src/main/clojure/hikesaber/benchmark/harness.clj
@@ -6,20 +6,23 @@
              ])
   (:require [hikesaber.divvy-ride-records :as records]))
 
-(defn -loadRecords [] records/loaded)
+(defn -loadRecords []
+  (let [ret records/loaded]
+    (count ret) ;; force loading
+    ret))
 
+;; Result "countUniqueBikes":
+;;   0.666 ±(99.9%) 0.003 s/op [Average]
+;;   (min, avg, max) = (0.642, 0.666, 0.732), stdev = 0.013
+;;   CI (99.9%): [0.663, 0.670] (assumes normal distribution)
+;; 
+;; 
+;; # Run complete. Total time: 01:02:18
+;; 
+;; Benchmark                  Mode  Cnt  Score   Error  Units
+;; Records.countUniqueBikes  thrpt  200  1.470 ± 0.009  ops/s
+;; Records.countUniqueBikes   avgt  200  0.666 ± 0.003   s/op
 
-;;Result "countUniqueBikes":
-;;  0.658 ±(99.9%) 0.003 s/op [Average]
-;;  (min, avg, max) = (0.633, 0.658, 0.731), stdev = 0.015
-;;  CI (99.9%): [0.655, 0.662] (assumes normal distribution)
-;;
-;;
-;;# Run complete. Total time: 01:02:55
-;;
-;;Benchmark                  Mode  Cnt  Score   Error  Units
-;;Records.countUniqueBikes  thrpt  200  1.491 ± 0.008  ops/s
-;;Records.countUniqueBikes   avgt  200  0.658 ± 0.003   s/op
 (defn -countUniqueBikes [records]
   (loop [coll records
          ids #{}]
